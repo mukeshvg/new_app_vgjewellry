4a5edc7897e5a389131686456217d838b85d17c0
mitesh <miteshthakur7@gmail.com>
2025-11-12 19:51:16 +0530

    ideal stock added

---
+++ vgjewellry/ideal_stock_api.py
@@ -0,0 +1,456 @@
+import frappe
+import pyodbc
+import os
+import pandas as pd
+
+
+value = os.getenv('sjodbc')
+
+def connect():
+    conn = pyodbc.connect(value, autocommit=True)
+    conn.set_attr(pyodbc.SQL_ATTR_TXN_ISOLATION, pyodbc.SQL_TXN_READ_UNCOMMITTED)
+    return conn
+
+@frappe.whitelist(allow_guest=True)
+def get_ideal_stock(from_date, to_date):
+    #get_stock_data(from_date,to_date)
+    #get_sale_data(from_date,to_date)
+    return calculate_ideal_stock()
+
+def get_stock_data(from_date, to_date):
+    con = connect()
+    cursor = con.cursor()
+
+    full_query = r'''
+    IF OBJECT_ID('tempdb..#LabelData') IS NOT NULL DROP TABLE #LabelData;
+    IF OBJECT_ID('tempdb..#LabelWithRange') IS NOT NULL DROP TABLE #LabelWithRange;
+
+    DECLARE 
+        @FromDate DATE = (?),
+        @ToDate   DATE = (?);
+
+    SELECT 
+        lb.LabelNo,
+        lb.NetWt,
+        lb.ItemMstID,
+        lb.BranchID,
+        lb.VouDate,
+        lb.NextVouDate,
+        lt.VarietyMstId,
+        im.ItemName,
+        vm.VarietyName,
+        bm.BranchName AS BranchShortCode
+    INTO #LabelData
+    FROM [D:\ORNNX\ORNATENXDATA\DATA\SVGL\SVGL.MDF].dbo.LabelDtWiseBal AS lb
+    INNER JOIN dbo.LabelTransaction AS lt ON lb.LabelNo = lt.LabelNo
+    LEFT JOIN dbo.ItemMst AS im ON im.ItemMstID = lt.ItemMstID
+    LEFT JOIN dbo.VarietyMst AS vm ON vm.VarietyMstId = lt.VarietyMstId
+    LEFT JOIN dbo.BranchMaster AS bm ON bm.BranchID = lb.BranchID
+    WHERE lb.NetWt > 0
+      AND lb.ItemTradMstID = 1002
+      AND lb.NextVouDate >= @FromDate
+      AND lb.VouDate <= @ToDate;
+
+    SELECT
+        l.BranchID,
+        l.ItemMstID,
+        l.VarietyMstId,
+        l.ItemName,
+        l.VarietyName,
+        l.BranchShortCode,
+        CASE 
+            WHEN l.NetWt <= 5 THEN '0-5'
+            WHEN l.NetWt <= 10 THEN '5.001-10'
+            WHEN l.NetWt <= 15 THEN '10.001-15'
+            WHEN l.NetWt <= 20 THEN '15.001-20'
+            WHEN l.NetWt <= 25 THEN '20.001-25'
+            WHEN l.NetWt <= 30 THEN '25.001-30'
+            WHEN l.NetWt <= 35 THEN '30.001-35'
+            WHEN l.NetWt <= 40 THEN '35.001-40'
+            WHEN l.NetWt <= 45 THEN '40.001-45'
+            WHEN l.NetWt <= 50 THEN '45.001-50'
+            WHEN l.NetWt <= 55 THEN '50.001-55'
+            WHEN l.NetWt <= 60 THEN '55.001-60'
+            WHEN l.NetWt <= 65 THEN '60.001-65'
+            WHEN l.NetWt <= 70 THEN '65.001-70'
+            WHEN l.NetWt <= 75 THEN '70.001-75'
+            WHEN l.NetWt <= 80 THEN '75.001-80'
+            WHEN l.NetWt <= 85 THEN '80.001-85'
+            WHEN l.NetWt <= 90 THEN '85.001-90'
+            WHEN l.NetWt <= 95 THEN '90.001-95'
+            WHEN l.NetWt <= 100 THEN '95.001-100'
+            WHEN l.NetWt <= 120 THEN '100.001-120'
+            WHEN l.NetWt <= 140 THEN '120.001-140'
+            WHEN l.NetWt <= 160 THEN '140.001-160'
+            WHEN l.NetWt <= 180 THEN '160.001-180'
+            WHEN l.NetWt <= 200 THEN '180.001-200'
+            WHEN l.NetWt <= 220 THEN '200.001-220'
+            WHEN l.NetWt <= 240 THEN '220.001-240'
+            WHEN l.NetWt <= 260 THEN '240.001-260'
+            WHEN l.NetWt <= 280 THEN '260.001-280'
+            WHEN l.NetWt <= 300 THEN '280.001-300'
+            WHEN l.NetWt <= 320 THEN '300.001-320'
+            WHEN l.NetWt <= 340 THEN '320.001-340'
+            WHEN l.NetWt <= 360 THEN '340.001-360'
+            WHEN l.NetWt <= 380 THEN '360.001-380'
+            ELSE '>380'
+        END AS WeightRange,
+        COUNT(DISTINCT l.LabelNo) AS LabelCount,
+        SUM(l.NetWt) AS TotalNetWt,
+        SUM(
+            CASE 
+                WHEN DATEDIFF(DAY,
+                    CASE WHEN l.VouDate < @FromDate THEN @FromDate ELSE l.VouDate END,
+                    CASE WHEN l.NextVouDate > @ToDate THEN @ToDate ELSE l.NextVouDate END
+                ) + 1 > DATEDIFF(DAY, @FromDate, @ToDate) + 1
+                THEN DATEDIFF(DAY, @FromDate, @ToDate) + 1
+                ELSE DATEDIFF(DAY,
+                    CASE WHEN l.VouDate < @FromDate THEN @FromDate ELSE l.VouDate END,
+                    CASE WHEN l.NextVouDate > @ToDate THEN @ToDate ELSE l.NextVouDate END
+                ) + 1
+            END
+        ) AS DaysActive
+    INTO #LabelWithRange
+    FROM #LabelData l
+    GROUP BY
+        l.BranchID, l.ItemMstID, l.VarietyMstId, l.ItemName, l.VarietyName, l.BranchShortCode,
+        CASE 
+            WHEN l.NetWt <= 5 THEN '0-5'
+            WHEN l.NetWt <= 10 THEN '5.001-10'
+            WHEN l.NetWt <= 15 THEN '10.001-15'
+            WHEN l.NetWt <= 20 THEN '15.001-20'
+            WHEN l.NetWt <= 25 THEN '20.001-25'
+            WHEN l.NetWt <= 30 THEN '25.001-30'
+            WHEN l.NetWt <= 35 THEN '30.001-35'
+            WHEN l.NetWt <= 40 THEN '35.001-40'
+            WHEN l.NetWt <= 45 THEN '40.001-45'
+            WHEN l.NetWt <= 50 THEN '45.001-50'
+            WHEN l.NetWt <= 55 THEN '50.001-55'
+            WHEN l.NetWt <= 60 THEN '55.001-60'
+            WHEN l.NetWt <= 65 THEN '60.001-65'
+            WHEN l.NetWt <= 70 THEN '65.001-70'
+            WHEN l.NetWt <= 75 THEN '70.001-75'
+            WHEN l.NetWt <= 80 THEN '75.001-80'
+            WHEN l.NetWt <= 85 THEN '80.001-85'
+            WHEN l.NetWt <= 90 THEN '85.001-90'
+            WHEN l.NetWt <= 95 THEN '90.001-95'
+            WHEN l.NetWt <= 100 THEN '95.001-100'
+            WHEN l.NetWt <= 120 THEN '100.001-120'
+            WHEN l.NetWt <= 140 THEN '120.001-140'
+            WHEN l.NetWt <= 160 THEN '140.001-160'
+            WHEN l.NetWt <= 180 THEN '160.001-180'
+            WHEN l.NetWt <= 200 THEN '180.001-200'
+            WHEN l.NetWt <= 220 THEN '200.001-220'
+            WHEN l.NetWt <= 240 THEN '220.001-240'
+            WHEN l.NetWt <= 260 THEN '240.001-260'
+            WHEN l.NetWt <= 280 THEN '260.001-280'
+            WHEN l.NetWt <= 300 THEN '280.001-300'
+            WHEN l.NetWt <= 320 THEN '300.001-320'
+            WHEN l.NetWt <= 340 THEN '320.001-340'
+            WHEN l.NetWt <= 360 THEN '340.001-360'
+            WHEN l.NetWt <= 380 THEN '360.001-380'
+            ELSE '>380'
+        END;
+
+    SELECT 
+        BranchID,
+        ItemMstID,
+        VarietyMstId,
+        ItemName,
+        VarietyName,
+        BranchShortCode,
+        WeightRange,
+        LabelCount,
+        TotalNetWt,
+        CASE 
+            WHEN DaysActive > DATEDIFF(DAY, @FromDate, @ToDate) + 1 
+            THEN DATEDIFF(DAY, @FromDate, @ToDate) + 1
+            ELSE DaysActive
+        END AS DaysActive
+    FROM #LabelWithRange
+    ORDER BY 
+        BranchID,
+        ItemMstID,
+        VarietyMstId,
+        TRY_CAST(LEFT(WeightRange, CHARINDEX('-', WeightRange + '-') - 1) AS DECIMAL(10,3));
+    '''
+
+    cursor.execute(full_query, (from_date, to_date))
+
+    # Skip all intermediate non-result sets until we reach the final SELECT
+    while True:
+        try:
+            if cursor.description:  # means this set has columns => it's the final SELECT
+                break
+            if not cursor.nextset():  # no more sets
+                break
+        except pyodbc.ProgrammingError:
+            break
+
+    # Now fetch the actual data
+    rows = cursor.fetchall()
+    
+    frappe.db.sql("TRUNCATE TABLE `tabStockDataForIdeal`")
+    frappe.db.commit()
+
+    fields = [
+        "name",    
+        "branch_id",
+        "item_id",
+        "variety_id",
+        "item",
+        "variety",
+        "branch",
+        "weight_range",
+        "label_count",
+        "total_net_wt",
+        "days_present",
+        "avg_stock"
+    ]
+
+    # Convert fetched data into list of tuples
+
+    values = []
+    for i, row in enumerate(rows, start=1):
+        total_net_wt=row[8]
+        days_present=row[9]
+        avg_stock=round((total_net_wt/days_present),2)
+        values.append((str(i),) + tuple(row)+(avg_stock,))
+
+    # Insert all records quickly
+    frappe.db.bulk_insert("StockDataForIdeal", fields, values)
+
+    frappe.db.commit()
+    
+    cursor.close()
+    con.close()
+    return f"{len(values)} records inserted successfully123"
+
+def get_sale_data(from_date, to_date):
+    con = connect()
+    cursor = con.cursor()
+
+    full_query = r'''
+    ;WITH Base AS (
+    SELECT
+        s.BranchID,
+        bm.BranchName,
+        s.ItemMstID,
+        im.ItemName,
+        s.VarietyMstID,
+        vm.VarietyName,
+        s.LabelNo,
+        s.NetWt,
+        CASE
+            WHEN s.NetWt BETWEEN 0 AND 5 THEN '0-5'
+            WHEN s.NetWt BETWEEN 5.001 AND 10 THEN '5.001-10'
+            WHEN s.NetWt BETWEEN 10.001 AND 15 THEN '10.001-15'
+            WHEN s.NetWt BETWEEN 15.001 AND 20 THEN '15.001-20'
+            WHEN s.NetWt BETWEEN 20.001 AND 25 THEN '20.001-25'
+            WHEN s.NetWt BETWEEN 25.001 AND 30 THEN '25.001-30'
+            WHEN s.NetWt BETWEEN 30.001 AND 35 THEN '30.001-35'
+            WHEN s.NetWt BETWEEN 35.001 AND 40 THEN '35.001-40'
+            WHEN s.NetWt BETWEEN 40.001 AND 45 THEN '40.001-45'
+            WHEN s.NetWt BETWEEN 45.001 AND 50 THEN '45.001-50'
+            WHEN s.NetWt BETWEEN 50.001 AND 55 THEN '50.001-55'
+            WHEN s.NetWt BETWEEN 55.001 AND 60 THEN '55.001-60'
+            WHEN s.NetWt BETWEEN 60.001 AND 65 THEN '60.001-65'
+            WHEN s.NetWt BETWEEN 65.001 AND 70 THEN '65.001-70'
+            WHEN s.NetWt BETWEEN 70.001 AND 75 THEN '70.001-75'
+            WHEN s.NetWt BETWEEN 75.001 AND 80 THEN '75.001-80'
+            WHEN s.NetWt BETWEEN 80.001 AND 85 THEN '80.001-85'
+            WHEN s.NetWt BETWEEN 85.001 AND 90 THEN '85.001-90'
+            WHEN s.NetWt BETWEEN 90.001 AND 95 THEN '90.001-95'
+            WHEN s.NetWt BETWEEN 95.001 AND 100 THEN '95.001-100'
+            WHEN s.NetWt BETWEEN 100.001 AND 120 THEN '100.001-120'
+            WHEN s.NetWt BETWEEN 120.001 AND 140 THEN '120.001-140'
+            WHEN s.NetWt BETWEEN 140.001 AND 160 THEN '140.001-160'
+            WHEN s.NetWt BETWEEN 160.001 AND 180 THEN '160.001-180'
+            WHEN s.NetWt BETWEEN 180.001 AND 200 THEN '180.001-200'
+            WHEN s.NetWt BETWEEN 200.001 AND 220 THEN '200.001-220'
+            WHEN s.NetWt BETWEEN 220.001 AND 240 THEN '220.001-240'
+            WHEN s.NetWt BETWEEN 240.001 AND 260 THEN '240.001-260'
+            WHEN s.NetWt BETWEEN 260.001 AND 280 THEN '260.001-280'
+            WHEN s.NetWt BETWEEN 280.001 AND 300 THEN '280.001-300'
+            WHEN s.NetWt BETWEEN 300.001 AND 320 THEN '300.001-320'
+            WHEN s.NetWt BETWEEN 320.001 AND 340 THEN '320.001-340'
+            WHEN s.NetWt BETWEEN 340.001 AND 360 THEN '340.001-360'
+            WHEN s.NetWt BETWEEN 360.001 AND 380 THEN '360.001-380'
+            ELSE '>380'
+        END AS WeightRange
+    FROM [D:\ORNNX\ORNATENXDATA\DATA\SVGL\SVGL.MDF].dbo.SPTran AS s
+    LEFT JOIN dbo.ItemMst im ON s.ItemMstID = im.ItemMstID
+    LEFT JOIN dbo.VarietyMst vm ON s.VarietyMstID = vm.VarietyMstId
+    LEFT JOIN dbo.BranchMaster bm ON s.BranchID = bm.BranchID
+    WHERE s.VouType = 'SL'
+      AND s.VouDate >= (?)
+      AND s.VouDate <= (?)
+      AND s.ItemTradMstID = 1002
+)
+SELECT
+    BranchID,
+    BranchName,
+    ItemMstID,
+    ItemName,
+    VarietyMstID,
+    VarietyName,
+    WeightRange,
+    COUNT(*) AS LabelCount,
+    SUM(NetWt) AS TotalNetWt
+FROM Base
+GROUP BY
+    BranchID,
+    BranchName,
+    ItemMstID,
+    ItemName,
+    VarietyMstID,
+    VarietyName,
+    WeightRange
+ORDER BY
+    BranchID,
+    ItemMstID,
+    VarietyMstID,
+    TRY_CAST(LEFT(WeightRange, CHARINDEX('-', WeightRange + '-') - 1) AS DECIMAL(10,3));
+
+    '''
+
+    cursor.execute(full_query, (from_date, to_date))
+
+    # Skip all intermediate non-result sets until we reach the final SELECT
+    while True:
+        try:
+            if cursor.description:  # means this set has columns => it's the final SELECT
+                break
+            if not cursor.nextset():  # no more sets
+                break
+        except pyodbc.ProgrammingError:
+            break
+
+    # Now fetch the actual data
+    rows = cursor.fetchall()
+    
+    frappe.db.sql("TRUNCATE TABLE `tabSaleDataForIdeal`")
+    frappe.db.commit()
+
+    fields = [
+        "name",    
+        "branch_id",
+        "branch",
+        "item_id",
+        "item",
+        "variety_id",
+        "variety",
+        "weight_range",
+        "label_count",
+        "total_net_wt",
+    ]
+
+    # Convert fetched data into list of tuples
+
+    values = []
+    for i, row in enumerate(rows, start=1):
+        values.append((str(i),) + tuple(row))
+
+    # Insert all records quickly
+    frappe.db.bulk_insert("SaleDataForIdeal", fields, values)
+
+    frappe.db.commit()
+
+    cursor.close()
+    con.close()
+
+    return f"{len(values)} records inserted successfully456"
+
+
+import json
+
+def calculate_ideal_stock():
+    query = """
+    SELECT branch, item, variety, weight_range, avg_stock
+    FROM `tabStockDataForIdeal`
+"""
+    rows = frappe.db.sql(query, as_dict=True)
+    stock_data = [[row["branch"], row["item"], row["variety"], row["weight_range"], row["avg_stock"]] for row in rows]
+
+    query1 = """
+    SELECT branch, item, variety, weight_range, total_net_wt
+    FROM `tabSaleDataForIdeal`
+"""
+    rows1 = frappe.db.sql(query1, as_dict=True)
+    sales_data = [[row["branch"], row["item"], row["variety"], row["weight_range"], row["total_net_wt"]] for row in rows1]
+
+
+    sales_df = pd.DataFrame(sales_data, columns=["Branch", "Item", "Variety", "Weight_Range",  "Sales_Weight"])
+    stock_df = pd.DataFrame(stock_data, columns=["Branch","Item", "Variety", "Weight_Range",  "Stock_Weight"])
+
+
+    df = pd.merge(sales_df, stock_df, on=["Branch", "Item", "Variety", "Weight_Range"], how="left")
+
+    # --- Convert to numeric safely ---
+    df["Sales_Weight"] = pd.to_numeric(df["Sales_Weight"], errors='coerce').fillna(0)
+    df["Stock_Weight"] = pd.to_numeric(df["Stock_Weight"], errors='coerce').fillna(0)
+
+
+    # --- Calculate STR safely ---
+    df["STR"] = df.apply(lambda x: round(x["Sales_Weight"] / x["Stock_Weight"], 2) if x["Stock_Weight"] > 0 else 0, axis=1)
+    
+
+    df["GAST"] = df.groupby(["Branch", "Item", "Variety"]).apply(
+    lambda g: g.assign(GAST=(g["Sales_Weight"].sum() / g["Stock_Weight"].sum() if g["Stock_Weight"].sum() > 0 else 0))
+)["GAST"].values
+
+
+    # --- Calculate GAST safely ---
+    #df["GAST"] = df.groupby(["Branch", "Item", "Variety"]).apply(
+    #    lambda g: pd.Series(
+    #        g["Sales_Weight"].sum() / g["Stock_Weight"].sum() if g["Stock_Weight"].sum() > 0 else 0,
+    #        index=g.index
+    #    )
+    #)
+
+
+
+    # --- Group average sales ---
+    df["Group_Avg_Sales"] = df.groupby(["Branch", "Item", "Variety"])["Sales_Weight"].transform("mean")
+
+    # --- Add Serial Number ---
+    df.insert(0, "S.No", range(1, len(df) + 1))
+
+    # --- Return the DataFrame itself (not a string!) ---
+
+    def quadrant(row):
+        if row["Sales_Weight"] >= row["Group_Avg_Sales"] and row["STR"] >= row["GAST"]:
+            return "Q4 – High Appeal"
+        elif row["Sales_Weight"] < row["Group_Avg_Sales"] and row["STR"] >= row["GAST"]:
+            return "Q3 – Evolving"
+        elif row["Sales_Weight"] >= row["Group_Avg_Sales"] and row["STR"] < row["GAST"]:
+            return "Q2 – Saturated"
+        else:
+            return "Q1 – Low Appeal"
+
+    df["Quadrant"] = df.apply(quadrant, axis=1)
+
+
+    def ideal_stock(row):
+        if row["Quadrant"] == "Q1 – Low Appeal":
+            return row["Stock_Weight"] * 0.3
+        elif row["Quadrant"] == "Q2 – Saturated":
+            return row["Sales_Weight"] / row["GAST"]
+        else:
+            return row["Stock_Weight"]
+
+    df["Ideal_Stock"] = df.apply(ideal_stock, axis=1)
+
+
+    # --- Step 8: Calculate difference (for stock correction) ---
+    df["Correction"] = df["Ideal_Stock"] - df["Stock_Weight"]
+    data = df[["Branch", "Item", "Variety", "Weight_Range", "Sales_Weight", "Stock_Weight","STR", "GAST", "Quadrant", "Ideal_Stock", "Correction"]].values.tolist()
+    columns = [
+    "Branch", "Category", "Subcategory", "Weight_Range",
+    "Sales_Weight", "Stock_Weight", "STR", "GAST",
+    "Quadrant", "Ideal_Stock", "Correction"
+]
+    dict_list = [dict(zip(columns, row)) for row in data]
+    return dict_list
+
+
---
+++ vgjewellry/vg_jewellery/page/ideal_stock/ideal_stock.js
@@ -0,0 +1,305 @@
+frappe.pages['ideal-stock'].on_page_load = function(wrapper) {
+	var page = frappe.ui.make_app_page({
+		parent: wrapper,
+		title: 'Ideal Stock',
+		single_column: true
+	});
+	// --- CSS Styling ---
+	const style = `
+	<style>
+	    .date-range-container {
+		display: flex;
+		align-items: center;
+		flex-wrap: wrap;
+		gap: 10px;
+		background: #f8fafc;
+		padding: 14px;
+		border-radius: 10px;
+		box-shadow: 0 2px 8px rgba(0,0,0,0.06);
+		margin-top: 10px;
+	    }
+	    .dr-field {
+		display: flex;
+		align-items: center;
+		gap: 6px;
+		background: #fff;
+		padding: 6px 10px;
+		border: 1px solid #e2e8f0;
+		border-radius: 8px;
+	    }
+	    .dr-field label {
+		font-size: 13px;
+		color: #64748b;
+	    }
+	    .dr-field input[type="date"] {
+		border: none;
+		outline: none;
+		font-size: 14px;
+		background: transparent;
+		color: #0f172a;
+	    }
+	    .dr-submit {
+		background: linear-gradient(180deg, #2563eb, #1e40af);
+		color: #fff;
+		border: none;
+		padding: 8px 16px;
+		border-radius: 8px;
+		font-weight: 600;
+		cursor: pointer;
+		box-shadow: 0 4px 10px rgba(37,99,235,0.2);
+		transition: transform 0.1s ease;
+	    }
+	    .dr-submit:active { transform: scale(0.98); }
+	    .dr-result {
+		margin-top: 14px;
+		background: #fff;
+		border-radius: 8px;
+		padding: 12px;
+		border: 1px solid #e2e8f0;
+		box-shadow: 0 1px 4px rgba(0,0,0,0.04);
+		display: none;
+	    }
+	</style>
+    `;
+	$(page.body).append(style);
+
+	// --- HTML Layout ---
+	const html = `
+	<div class="date-range-container">
+	    <div class="dr-field">
+		<label for="from_date">From</label>
+		<input type="date" id="from_date">
+	    </div>
+	    <div class="dr-field">
+		<label for="to_date">To</label>
+		<input type="date" id="to_date">
+	    </div>
+	    <button class="dr-submit" id="fetch_btn">Submit</button>
+	</div>
+	<div class="dr-result" id="result_box"></div>
+	<div id="pivotContainer"  style=" width: 100%;"></div>
+    `;
+	$(page.body).append(html);
+
+	// --- JS Logic ---
+	const fromInput = $("#from_date");
+	const toInput = $("#to_date");
+	const resultBox = $("#result_box");
+
+	// Default range: last 7 days
+	const today = frappe.datetime.get_today();
+	const weekAgo = frappe.datetime.add_days(today, -225);
+
+	fromInput.val(weekAgo);
+	toInput.val(today);
+
+	function formatDateToDMY(isoDate) {
+		const [y, m, d] = isoDate.split("-");
+		return `${d}-${m}-${y}`;
+	}
+
+	function formatDateToISO(dmy) {
+		const [d, m, y] = dmy.split("-");
+		return `${y}-${m}-${d}`;
+	}
+
+	$("#fetch_btn").on("click", function () {
+		//debugger;
+        var usr = frappe.session.user;
+        var bu = frappe.pages[Object.keys(frappe.pages)[0]].baseURI;
+        let rpt = bu.substring(bu.lastIndexOf('/') + 1).trim();
+
+		 frappe.call({
+                method: "vgjewellry.VG_api.ucr_fetch",
+                // args: {"rpt":rpt},
+                args: {"rpt": rpt,"usr": usr},
+                callback: function (res) {
+			window.ucrformats = res.message;
+			console.log(res.message);
+                }
+        });
+
+		console.log(usr,bu,rpt);
+
+		let from_iso = fromInput.val();
+		let to_iso = toInput.val();
+
+		if (!from_iso || !to_iso) {
+			frappe.msgprint("Please select both dates");
+			return;
+		}
+
+		// Convert to D-M-Y for display
+		const from_dmy = formatDateToDMY(from_iso);
+		const to_dmy = formatDateToDMY(to_iso);
+
+		// Validate range
+		if (from_iso > to_iso) {
+			frappe.msgprint("Start date cannot be after End date");
+			return;
+		}
+
+		resultBox.hide().html(`<div>Fetching data from <b>${from_dmy}</b> to <b>${to_dmy}</b>...</div>`);
+
+		// Example backend call
+		frappe.call({
+			method: "vgjewellry.ideal_stock_api.get_ideal_stock", 
+			args: {
+				from_date: from_iso,  
+				to_date: to_iso
+			},
+			callback: function (r) {
+				console.log(r);
+				if (r.message) {
+					stdPivotUi(r.message);
+					/*resultBox
+						.html(`<div><b>From:</b> ${from_dmy} <b>To:</b> ${to_dmy}</div><pre>${JSON.stringify(r.message, null, 2)}</pre>`)
+						.show();*/
+				} else {
+					resultBox.html("<div>No data found</div>").show();
+				}
+			}
+		});
+	});
+
+	function stdPivotUi(pivotdata) {
+		$(function () {
+			frappe.require([
+				"/assets/vgjewellry/js/custom/flexmonster_cdn.js"
+			], () => {
+				let webori = window.location.origin;
+				if (webori.search("10.10.10.137") > 0) {
+					window.flexlickey = "Z7UN-162H69-6T1N31-1X1Z09-4K2L1E-38615B-1S386C-4P4S1Q-6T2J2O-6N4K47-6I5Y24-114S6V-6F6I16-0Y1F5U-3X0I0N-20253O-3I2O47-2M"
+				}
+				else if(webori.search("app.vgjewellry.com") > 0) {
+					window.flexlickey = "Z7T6-XJ6G1I-57443C-4M4W42-5Y0Q2E-2L1W47-076L30-0M0A6W-6Y182U-3Y3C59-5A4B"
+				}
+
+				 let selobj = {}
+                        let rpt = {}
+                        let custom_note = ""
+
+
+                        if (window.setreportname) {
+                                selobj = findNested(ucrformats, "sub_reportname", window.setreportname)
+                                rpt = JSON.parse(selobj?.report_format)
+                        }
+else {
+                                selobj = ucrformats.length ? ucrformats[0] : {}
+                                rpt = ucrformats.length ? JSON.parse(ucrformats[0]?.report_format) : {}
+                                window.setreportname = selobj?.sub_reportname
+                                let f = 0;
+                                for (let i = 0; i < window.ucrformats.length; i++) {
+                                        if (window.ucrformats[i]["is_default"] && window.ucrformats[i]["username"] == frappe.session.user && window.ucrformats[i]["is_global"] == 0) {
+                                                f = 1;
+                                                selobj = window.ucrformats[i]
+                                                rpt = JSON.parse(window.ucrformats[i]?.report_format)
+                                                window.setreportname = selobj?.sub_reportname
+                                                break;
+                                        }
+                                }
+
+                                if (f === 0) {
+                                        for (let i = 0; i < window.ucrformats.length; i++) {
+                                                if (window.ucrformats[i]["is_default"] && window.ucrformats[i]["is_global"]) {
+                                                        selobj = window.ucrformats[i]
+                                                        rpt = JSON.parse(ucrformats[i]?.report_format)
+                                                        window.setreportname = selobj?.sub_reportname
+                                                        break;
+                                                }
+                                        }
+                                }
+                        }
+
+				let baseConfig = {
+					container: "pivotContainer",
+					componentFolder: "https://cdn.flexmonster.com/",
+					licenseKey: flexlickey,
+					toolbar: true,
+					//beforetoolbarcreated: customizeToolbar,
+					height: 660,
+					report: {
+						dataSource: {
+							data: pivotdata
+						},
+						formats: [{
+							name: "",
+							thousandsSeparator: ",",
+							nullValue: "",
+							infinityValue: "",
+							divideByZeroValue: ""
+						}]
+					}
+				};
+				// If we have a saved report format, merge it with base config
+				if (Object.keys(rpt).length !== 0) {
+					baseConfig.report = {
+						...baseConfig.report,
+						slice: rpt.slice,
+						options: rpt.options,
+						conditions: rpt.conditions,
+						formats: rpt.formats || baseConfig.report.formats,
+						tableSizes: rpt.tableSizes,
+						customFields: rpt.customFields,
+					};
+				}
+
+				// Initialize Flexmonster with consistent configuration
+				let pivot = new Flexmonster(baseConfig);
+
+				// Add event listener for report complete
+				pivot.on("reportcomplete", function() {
+					if (rpt.options && rpt.options.viewType) {
+						pivot.setViewType(rpt.options.viewType);
+					}
+					if (rpt.slice && rpt.slice.expandAll) {
+						pivot.expandAllData();
+					}
+				});
+				pivot.customizeContextMenu(function (items, data) {
+					let cell = flexmonster.getSelectedCell();
+					let param = cell.member?.hierarchyName;
+					if (param) {
+						items.push({
+							label: "Expand " + param + " Data",
+							handler: function () {
+								pivot.expandData(param);
+							}
+						});
+						items.push({
+							label: "Collapse " + param + " Data",
+							handler: function () {
+								pivot.collapseData(param);
+							}
+						});
+					}
+					items.push({
+						label: "Expand All Data",
+						handler: function () {
+							pivot.expandAllData();
+						}
+					});
+					items.push({
+						label: "Collapse All Data",
+						handler: function () {
+							pivot.collapseAllData();
+						}
+					});
+					items.push({
+						label: "Show AverageTotal",
+						handler: function () {
+							showAverageTotal();
+						}
+					});
+					return items;
+				});
+				if (window.ucrformats.length > 0 && Object.keys(selobj).length !== 0) {
+                                $('.report-button').removeClass('selected');
+                                $(`#${window.setreportname}`).addClass('selected');
+                        }
+
+
+			})
+		})
+	}	
+}
