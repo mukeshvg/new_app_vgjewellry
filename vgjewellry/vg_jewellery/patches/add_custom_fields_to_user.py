bbe7e903d275a248e1afc6c26546e0174eca1dff
mitesh <miteshthakur7@gmail.com>
2025-12-05 15:29:50 +0530

    pathc

--- vgjewellry/vg_jewellery/patches/add_custom_fields_to_user.py
+++ vgjewellry/vg_jewellery/patches/add_custom_fields_to_user.py
@@ -4,8 +4,15 @@ import frappe
 def execute():
     custom_fields = [
         {
-            "fieldname": "branch",
+            "fieldname": "ornate_branch",
             "label": "Branch",
+            "fieldtype": "Link",
+            "insert_after": "username",
+            "options": "Ornate_Branch_Master"
+        },
+        {
+            "fieldname": "branch",
+            "label": "Branch Old",
             "fieldtype": "Link",
             "insert_after": "username",
             "options": "Branch"
@@ -25,6 +32,23 @@ def execute():
             "options": "Branch Counter"
         }
     ]
+
+    doc_type = "User"  # Replace with actual DocType
+    field_name = "branch"  # Replace with actual custom field name
+
+    # Get the custom field record
+    custom_field = frappe.get_doc("Custom Field", {
+        'dt': doc_type,
+        'fieldname': field_name
+    })
+
+    # Change the label to a new name
+    custom_field.label = "Branch Old"  # Replace with desired label
+
+    # Save the changes
+    custom_field.save()
+
+    frappe.db.commit()
 
     for field in custom_fields:
         if not frappe.db.exists("Custom Field", {"dt": "User", "fieldname": field["fieldname"]}):
