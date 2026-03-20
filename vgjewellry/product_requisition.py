03ea78698517dc9b01259f95f31a5b437f5ca7db
mitesh <miteshthakur7@gmail.com>
2026-03-20 13:12:25 +0530

    change in variety

--- vgjewellry/product_requisition.py
+++ vgjewellry/product_requisition.py
@@ -294,7 +294,14 @@ def get_product_details_new_format(page=1, page_size=10, search=""):
                 sz_name=frappe.get_doc("Ornate_Size_Master",item.size)
                 size_id =item.size
                 size_name= sz_name.size
-            idea_stock=frappe.get_all("Current_Stock_Ideal_Stock",filters={'branch_id':user_data.ornate_branch,'item_id':item.item,'variety_id':item.variety,'weight_range':wt_name.weight_range},fields=['target_pcs','stock_pcs'],limit=1)
+            query ='''
+            SELECT variety_id, variety_name, item_trade_mst_id FROM tabOrnate_Variety_Master AS vm WHERE FIND_IN_SET((SELECT item_trade_mst_id  FROM tabOrnate_Item_Master  WHERE item_mst_id = %s), vm.item_trade_mst_id) > 0;
+    '''
+            vdata = frappe.db.sql(query, item.item, as_dict=True)
+            variety_ids = [vrow['variety_id'] for vrow in vdata]    
+
+            #idea_stock=frappe.get_all("Current_Stock_Ideal_Stock",filters={'branch_id':user_data.ornate_branch,'item_id':item.item,'variety_id':item.variety,'weight_range':wt_name.weight_range},fields=['target_pcs','stock_pcs'],limit=1)
+            idea_stock=frappe.get_all("Current_Stock_Ideal_Stock",filters={'branch_id':user_data.ornate_branch,'item_id':item.item,'variety_id':['in',variety_ids],'weight_range':wt_name.weight_range},fields=['target_pcs','stock_pcs'],limit=1)
             suggested=0;
             in_stock=0;
             if len(idea_stock)>0:
