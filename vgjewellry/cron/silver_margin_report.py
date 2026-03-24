9edd0ab7680c050bc043a192af8dab2ee4adbf56
mitesh <miteshthakur7@gmail.com>
2026-03-24 17:37:07 +0530

    chang

--- vgjewellry/cron/silver_margin_report.py
+++ vgjewellry/cron/silver_margin_report.py
@@ -74,7 +74,7 @@ def get_silver_margin_report_data():
     #to_date = filters.get("to_date")
     #from_date="2025-04-01"
     #to_date="2025-04-02"
-    from_date = "2025-01-15"
+    from_date = "2026-01-15"
     to_date   = "2026-03-31"
 
     result = frappe.db.sql("""
