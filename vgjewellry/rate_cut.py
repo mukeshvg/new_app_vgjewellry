8cdd8b50200a8ee63f6623d4f6b20a42e7297e97
mitesh <miteshthakur7@gmail.com>
2026-06-14 17:37:59 +0530

    changes in r

--- vgjewellry/rate_cut.py
+++ vgjewellry/rate_cut.py
@@ -176,7 +176,7 @@ def get_saved_transactions(acc_mst_id , summary_id):
             "is_completed": 0,
             "is_selected": 1
         },
-        fields=["item_tran_id"]
+        fields=["item_tran_id","fine_wt", "oc", "hm"]
     )
 
 @frappe.whitelist()
@@ -199,6 +199,7 @@ def save_rate_cut_summary(data):
             doc = frappe.new_doc("Rate Cut Summary")
 
         doc.vendor = d["vendor"]
+        doc.ratecut_date = d["ratecut_date"]
         doc.acc_mst_id = d["accMstId"]
         doc.fine_wt = d["ketanFineWt"]
         doc.fine_wt_selected = d["selectedFineWt"]
@@ -208,4 +209,72 @@ def save_rate_cut_summary(data):
 
         doc.save(ignore_permissions=True)
 
-    return "saved"    
+    return "saved"   
+
+@frappe.whitelist()
+def get_arihant_rate():
+    current_ts_ms = int(datetime.now().timestamp() * 1000)
+    url = f"https://bcast.arihantspot.com:7768/VOTSBroadcastStreaming/Services/xml/GetLiveRateByTemplateID/arihant?_={current_ts_ms}"
+
+    payload = {}
+    headers = {}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    dt = response.text
+    #match = re.search(r"GOLD 999 WITH GST IMP-IND.*?(\d+)", response)
+    match = re.search(r"GOLD 999 WITH GST\s*-\s*(\d+)", dt)
+
+    if match:
+        arihant_gold_rate = int(match.group(1))
+        arihant_gold_rate = round(arihant_gold_rate)
+    else:
+        arihant_gold_rate=0
+    return arihant_gold_rate    
+
+@frappe.whitelist()
+def save_single_rate_cut(row):
+
+    row = json.loads(row)
+
+    doc = frappe.get_doc(
+        "Rate Cut Summary",
+        row["name"]
+    )
+
+    doc.rate_999_with_gst = row.get("rate999WGst")
+    doc.rate_999_without_gst = row.get("rate999WithoutGst")
+    doc.bill_value_without_gst = row.get("billWithoutGst")
+    doc.bill_value_with_gst = row.get("withGstValue")
+    doc.bill_amt = row.get("billAmt")
+    doc.diff = row.get("diff")
+
+    doc.save(ignore_permissions=True)
+
+    return "saved"
+
+
+@frappe.whitelist()
+def delete_rate_cut_summary(summary_id):
+
+    transactions = frappe.get_all(
+        "Rate Cut Transaction",
+        filters={"summary_id": summary_id},
+        pluck="name"
+    )
+
+    for txn in transactions:
+        frappe.delete_doc(
+            "Rate Cut Transaction",
+            txn,
+            force=1
+        )
+
+    frappe.delete_doc(
+        "Rate Cut Summary",
+        summary_id,
+        force=1
+    )
+
+    frappe.db.commit()
+
+    return "deleted"
--- vgjewellry/vg_jewellery/page/rate_cut/rate_cut.js
+++ vgjewellry/vg_jewellery/page/rate_cut/rate_cut.js
@@ -23,7 +23,7 @@ frappe.pages['rate-cut'].on_page_load = function(wrapper) {
 
 	    .rate-cut-table th,
 	    .rate-cut-table td {
-		width: 120px;
+		width: 150px;
 		text-align: center;
 		vertical-align: middle;
 		word-wrap: break-word;
@@ -65,6 +65,22 @@ frappe.pages['rate-cut'].on_page_load = function(wrapper) {
 		    Add
 		</button>
 	    </div>
+	    <div class="col-md-4" style="margin-top:24px">
+	    <span id="arihant-rate-display"
+	  style="
+	    margin-left:20px;
+	    font-weight:bold;
+	    color:#198754;
+	    font-size:16px;
+	  ">
+	Rate: -
+    </span>
+
+    <button class="btn btn-warning ml-2"
+	    id="arihant-rate-btn">
+	Arihant Rate
+    </button>
+	    </div>
 
 	</div>
 
@@ -73,39 +89,55 @@ frappe.pages['rate-cut'].on_page_load = function(wrapper) {
 
 	function load_summary_table() {
 
-        frappe.call({
-            method: "frappe.client.get_list",
-            args: {
-                doctype: "Rate Cut Summary",
-                filters: { is_submitted: 0 },
-                fields: [
-                    "name",
-                    "vendor",
-                    "acc_mst_id",
-                    "fine_wt",
-                    "fine_wt_selected",
-                    "net_wt",
-                    "oc",
-                    "hm"
-                ]
-            },
-            callback: function (r) {
+		frappe.call({
+			method: "frappe.client.get_list",
+			args: {
+				doctype: "Rate Cut Summary",
+				filters: { is_submitted: 0 },
+				fields: [
+					"name",
+					"ratecut_date",
+					"vendor",
+					"acc_mst_id",
+					"fine_wt",
+					"fine_wt_selected",
+					"net_wt",
+					"oc",
+					"hm",
+					"rate_999_with_gst",
+					"rate_999_without_gst",
+					"bill_value_without_gst",
+					"bill_value_with_gst",
+					"bill_amt",
+					"diff"
+				]
+			},
+			callback: function (r) {
 
-                addedRows = (r.message || []).map(d => ({
-                    name: d.name,
-                    vendor: d.vendor,
-                    accMstId: d.acc_mst_id,
-                    ketanFineWt: d.fine_wt,
-                    selectedFineWt: d.fine_wt_selected,
-                    netWt: d.net_wt,
-                    oc: d.oc,
-                    hm: d.hm
-                }));
+				addedRows = (r.message || []).map(d => ({
+					name: d.name,
+					ratecut_date:d.ratecut_date,
+					vendor: d.vendor,
+					accMstId: d.acc_mst_id,
+					ketanFineWt: d.fine_wt,
+					selectedFineWt: d.fine_wt_selected,
+					netWt: d.net_wt,
+					oc: d.oc,
+					hm: d.hm,
+					rate999WGst:d.rate_999_with_gst,
+					rate999WithoutGst:d.rate_999_without_gst,
+					billWithoutGst:d.bill_value_without_gst,
+					withGstValue:d.bill_value_with_gst,
+					billAmt:d.bill_amt,
+					diff:d.diff
 
-                render_table();
-            }
-        });
-    }
+
+				}));
+
+				render_table();
+			}
+		});
+	}
 	function render_table() {
 
 		let totalKetanFineWt = 0;
@@ -120,18 +152,21 @@ frappe.pages['rate-cut'].on_page_load = function(wrapper) {
 
 			<thead>
 			    <tr>
+				<th>Ratecut Date</th>
 				<th>Vendor</th>
 				<th>Fine Wt (Ketan Sir)</th>
 				<th>Fine Wt Selected</th>
 				<th>Net Wt</th>
 				<th>HM</th>
 				<th>OC</th>
-				<th>RATE 999W.GST</th>
-				<th>999 W GST</th>
+				<th>RATE 999 With GST</th>
+				<th>RATE 999 Without GST</th>
 				<th>BILL VALUE WITHOUT GST</th>
 				<th>WITH GST VALUE</th>
 				<th>BILL AMT</th>
 				<th>Diff</th>
+				<th>Save</th>
+				<th>Remove</th>
 			    </tr>
 			</thead>
 
@@ -156,6 +191,9 @@ frappe.pages['rate-cut'].on_page_load = function(wrapper) {
 
 			html += `
 			<tr>
+				<td>${row.ratecut_date
+        ? moment(row.ratecut_date).format("DD-MM-YYYY")
+        : ''}</td>
 
 			    <td>
 				<span class="vendor-link"
@@ -180,18 +218,52 @@ frappe.pages['rate-cut'].on_page_load = function(wrapper) {
 				${row.oc.toFixed(2)}
 			    </td>
 
-			    <td>${row.rate999WGst}</td>
+			    <td><input type="number"
+	   class="form-control rate999wgst-input"
+	   data-index="${index}"
+	   value="${row.rate999WGst || ''}"
+	   step="0.01"></td>
 
-			    <td>${row.rate999WithoutGst}</td>
+			    <td> <input type="number" readonly
+	   class="form-control rate999wogst-input"
+	   data-index="${index}"
+	   value="${row.rate999WithoutGst || ''}"
+	   step="0.01"></td>
 
-			    <td>${row.billWithoutGst}</td>
+			    <td><input type="number" readonly
+	   class="form-control billwithoutgst-input"
+	   data-index="${index}"
+	   value="${row.billWithoutGst || ''}"
+	   step="0.01"></td>
 
-			    <td>${row.withGstValue}</td>
+			    <td><input type="number" readonly
+	   class="form-control withgstvalue-input"
+	   data-index="${index}"
+	   value="${row.withGstValue || ''}"
+	   step="0.01"></td>
 
-			    <td>${row.billAmt}</td>
+			    <td><input type="number"
+	   class="form-control billamt-input"
+	   data-index="${index}"
+	   value="${row.billAmt || ''}"
+	   step="0.01"></td>
 
-			    <td>${row.diff}</td>
+			    <td class="diff-cell"> ${(row.diff || 0).toFixed(2)}</td>
+<td>
+    <button
+	class="btn btn-success btn-sm save-row-btn"
+	data-index="${index}">
+	Save
+    </button>
+</td>
 
+<td>
+    <button
+	class="btn btn-danger btn-sm remove-row-btn"
+	data-index="${index}">
+	Remove
+    </button>
+</td>
 			</tr>
 		    `;
 		});
@@ -200,9 +272,9 @@ frappe.pages['rate-cut'].on_page_load = function(wrapper) {
 		html += `
 		    <tr style="
 			font-weight:bold;
-			background:#f5f5f5;
+			background:#ffff00;
 		    ">
-
+			<td></td>
 			<td>TOTAL</td>
 
 			<td>
@@ -229,6 +301,8 @@ frappe.pages['rate-cut'].on_page_load = function(wrapper) {
 			<td></td>
 			<td></td>
 			<td></td>
+			<td></td>
+			<td></td>
 
 		    </tr>
 		`;
@@ -239,6 +313,84 @@ frappe.pages['rate-cut'].on_page_load = function(wrapper) {
 		`;
 
 		$('#vendor-table').html(html);
+		$('#vendor-table .rate999wgst-input').on('change', function () {
+			let idx = $(this).data('index');
+
+			let gstRate = parseFloat($(this).val()) || 0;
+
+			addedRows[idx].rate999WGst = gstRate;
+
+			// 999 W GST / 103%
+			let withoutGst = gstRate / 1.03;
+
+			addedRows[idx].rate999WithoutGst = withoutGst;
+
+			// Update UI
+			$(`.rate999wogst-input[data-index="${idx}"]`)
+				.val(withoutGst.toFixed(2));
+			// Calculate Bill Value Without GST
+			let selectedFineWt =
+				parseFloat(addedRows[idx].selectedFineWt) || 0;
+
+			let hm =
+				parseFloat(addedRows[idx].hm) || 0;
+
+			let oc =
+				parseFloat(addedRows[idx].oc) || 0;
+
+			let billWithoutGst =
+				((withoutGst / 10) * selectedFineWt)
+				+ hm
+				+ oc;
+
+			addedRows[idx].billWithoutGst = billWithoutGst;
+
+			// Update UI
+			$(`.billwithoutgst-input[data-index="${idx}"]`)
+				.val(billWithoutGst.toFixed(2));
+			let withGstValue = billWithoutGst * 1.03;
+
+			addedRows[idx].withGstValue = withGstValue;
+
+			$(`.withgstvalue-input[data-index="${idx}"]`)
+				.val(withGstValue.toFixed(2));
+		});
+
+		/*$('#vendor-table .rate999wogst-input').on('change', function () {
+			let idx = $(this).data('index');
+			addedRows[idx].rate999WithoutGst = parseFloat($(this).val()) || 0;
+		});
+
+		$('#vendor-table .billwithoutgst-input').on('change', function () {
+			let idx = $(this).data('index');
+			addedRows[idx].billWithoutGst = parseFloat($(this).val()) || 0;
+		});
+
+		$('#vendor-table .withgstvalue-input').on('change', function () {
+			let idx = $(this).data('index');
+			addedRows[idx].withGstValue = parseFloat($(this).val()) || 0;
+		});*/
+
+		$('#vendor-table .billamt-input').on('change', function () {
+			let idx = $(this).data('index');
+
+			let billAmt = parseFloat($(this).val()) || 0;
+
+			addedRows[idx].billAmt = billAmt;
+
+			let withGstValue =
+				parseFloat(addedRows[idx].withGstValue) || 0;
+
+			let diff = billAmt - withGstValue;
+
+			addedRows[idx].diff = diff;
+
+			// Update Diff column
+			$(this)
+				.closest('tr')
+				.find('.diff-cell')
+				.text(diff.toFixed(2));
+		});
 
 		// Vendor Click
 		bind_vendor_click();
@@ -263,11 +415,17 @@ frappe.pages['rate-cut'].on_page_load = function(wrapper) {
 					summary_id: summary_id
 				},
 				callback: function(r) {
-
-					savedSelected = (r.message || []).map(
+					let savedRows = r.message || [];
+					savedSelected = savedRows.map(
 						x => x.item_tran_id
 					);
 
+
+					let savedMap = {};
+
+					savedRows.forEach(row => {
+						savedMap[row.item_tran_id] = row;
+					});
 					let vendorRecords =
 						apiData[rowData.accMstId];
 					console.log(vendorRecords);
@@ -275,16 +433,32 @@ frappe.pages['rate-cut'].on_page_load = function(wrapper) {
 					open_vendor_modal(
 						rowData,
 						vendorRecords,
-						savedSelected
+						savedSelected,
+						savedMap,
+						rowData.vendor
 					);
 				}
 			});
 		});
 	}
-	function open_vendor_modal(rowData, records,vendorName){
+	function open_vendor_modal(rowData, records,savedSelected,savedMap,vendorName){
 
 
 		let modalHtml = `
+		<div class="mb-3">
+	<b>Total FineWt:</b>
+	<span id="total-finewt">0.000</span>
+
+	&nbsp;&nbsp;&nbsp;
+
+	<b>Total OC:</b>
+	<span id="total-oc">0.00</span>
+
+	&nbsp;&nbsp;&nbsp;
+
+	<b>Total HM:</b>
+	<span id="total-hm">0.00</span>
+    </div>
 		    <div style="overflow-x:auto;">
 
 			<table class="
@@ -312,6 +486,7 @@ frappe.pages['rate-cut'].on_page_load = function(wrapper) {
 
 		records.forEach(record => {
 			let checked = savedSelected.includes(record.ItemTranID);
+			let saved = savedMap[record.ItemTranID] || {};
 			console.log(savedSelected);
 			console.log(record);
 
@@ -335,12 +510,24 @@ frappe.pages['rate-cut'].on_page_load = function(wrapper) {
 
 			    <td>${record.NetWt}</td>
 
-			    <td>${record.FineWt}</td>
+			    <td> <input type="number"
+	       class="form-control finewt-input"
+	       data-id="${record.ItemTranID}"
+	       value="${saved.fine_wt ?? record.FineWt ?? 0}"
+	       step="0.001"></td>
 
 			    <td>${record.Purity}</td>
 
-			    <td>${record.OtherCharge}</td>
-			    <td>${record.HM}</td>
+			    <td><input type="number"
+	   class="form-control oc-input"
+	   data-id="${record.ItemTranID}"
+	   value="${saved.oc ?? record.OtherCharge ?? 0}"
+	   step="0.01"></td>
+			    <td><input type="number"
+	   class="form-control hm-input"
+	   data-id="${record.ItemTranID}"
+	   value="${saved.hm ?? record.HM ?? 0}"
+	   step="0.01"></td>
 
 			    <td>${record.Narration}</td>
 
@@ -355,7 +542,7 @@ frappe.pages['rate-cut'].on_page_load = function(wrapper) {
 		`;
 
 		let d = new frappe.ui.Dialog({
-			title: vendorName + ' Records',
+			title: vendorName ,
 			size: 'extra-large',
 			fields: [
 				{
@@ -368,7 +555,49 @@ frappe.pages['rate-cut'].on_page_load = function(wrapper) {
 		d.fields_dict.vendor_records.$wrapper.html(
 			modalHtml
 		);
+		function updateTotals() {
 
+			let totalFineWt = 0;
+			let totalOC = 0;
+			let totalHM = 0;
+
+			d.$wrapper.find('.txn-check:checked').each(function () {
+
+				let id = $(this).data('id');
+
+				totalFineWt += parseFloat(
+					d.$wrapper.find(
+						`.finewt-input[data-id="${id}"]`
+					).val()
+				) || 0;
+
+				totalOC += parseFloat(
+					d.$wrapper.find(
+						`.oc-input[data-id="${id}"]`
+					).val()
+				) || 0;
+
+				totalHM += parseFloat(
+					d.$wrapper.find(
+						`.hm-input[data-id="${id}"]`
+					).val()
+				) || 0;
+			});
+
+			d.$wrapper.find('#total-finewt')
+				.text(totalFineWt.toFixed(3));
+
+			d.$wrapper.find('#total-oc')
+				.text(totalOC.toFixed(2));
+
+			d.$wrapper.find('#total-hm')
+				.text(totalHM.toFixed(2));
+		}
+		d.$wrapper.on(
+			'change',
+			'.txn-check, .finewt-input, .oc-input, .hm-input',
+			updateTotals
+		);
 		d.set_primary_action("Save Selection", function() {
 
 			let selectedRows = [];
@@ -380,8 +609,26 @@ frappe.pages['rate-cut'].on_page_load = function(wrapper) {
 				let row = records.find(r =>
 					r.ItemTranID == id
 				);
+				if (row) {
+					let fineWt = parseFloat(
+						d.$wrapper.find(`.finewt-input[data-id="${id}"]`).val()
+					) || 0;
 
-				if (row) selectedRows.push(row);
+					let oc = parseFloat(
+						d.$wrapper.find(`.oc-input[data-id="${id}"]`).val()
+					) || 0;
+
+					let hm = parseFloat(
+						d.$wrapper.find(`.hm-input[data-id="${id}"]`).val()
+					) || 0;
+
+					selectedRows.push({
+						...row,
+						FineWt: fineWt,	
+						OtherCharge: oc,
+						HM: hm
+					});
+				}
 			});
 
 			frappe.call({
@@ -401,6 +648,7 @@ frappe.pages['rate-cut'].on_page_load = function(wrapper) {
 				}
 			});
 		});
+		updateTotals();
 		d.show();
 
 	}
@@ -480,6 +728,71 @@ frappe.pages['rate-cut'].on_page_load = function(wrapper) {
 				render_input: true
 			});
 
+			/*$('#vendor-table1 .save-row-btn').on('click', function() {
+				let idx = $(this).data('index');
+
+				frappe.call({
+					method: "vgjewellry.rate_cut.save_single_rate_cut",
+					args: {
+						row: JSON.stringify(addedRows[idx])
+					},
+					callback: function () {
+						frappe.show_alert({
+							message: "Saved",
+							indicator: "green"
+						});
+					}
+				});
+			});
+			$('#vendor-table1 .remove-row-btn').on('click', function () {
+
+				let idx = $(this).data('index');
+
+				let row = addedRows[idx];
+
+				frappe.confirm(
+					'Remove this vendor?',
+					function () {
+
+						frappe.call({
+							method: "vgjewellry.rate_cut.delete_rate_cut_summary",
+							args: {
+								summary_id: row.name
+							},
+							callback: function () {
+
+								addedRows.splice(idx, 1);
+
+								render_table();
+
+								frappe.show_alert({
+									message: "Removed",
+									indicator: "red"
+								});
+							}
+						});
+
+					}
+				);
+			});*/
+			$('#arihant-rate-btn').on('click', function() {
+				frappe.call({
+					method: "vgjewellry.rate_cut.get_arihant_rate",
+					callback: function (r) {
+
+						if (r.message) {
+
+							$('#arihant-rate-display').text(
+								`Rate: ${r.message}`
+							);
+
+						} else {
+
+							frappe.msgprint("Rate not found");
+						}
+					}
+				});
+			});	
 			// Add Button
 			$('#add-row-btn').on('click', function() {
 
@@ -529,9 +842,11 @@ frappe.pages['rate-cut'].on_page_load = function(wrapper) {
 					totalOC +=
 						parseFloat(row.OtherCharge || 0);
 				})*/;
+				let today = frappe.datetime.get_today();
 
 				addedRows.push({
 					vendor: selectedName,
+					ratecut_date: today,
 					accMstId: accMstId,
 					ketanFineWt: fineWt,
 					selectedFineWt: totalFineWtSelected,
@@ -563,4 +878,48 @@ frappe.pages['rate-cut'].on_page_load = function(wrapper) {
 
 		}
 	});
+
+	$(document).on('click', '.save-row-btn', function () {
+
+    let idx = $(this).data('index');
+
+    frappe.call({
+        method: "vgjewellry.rate_cut.save_single_rate_cut",
+        args: {
+            row: JSON.stringify(addedRows[idx])
+        },
+        callback: function () {
+            frappe.show_alert({
+                message: "Saved",
+                indicator: "green"
+            });
+        }
+    });
+
+});
+	$(document).on('click', '.remove-row-btn', function () {
+
+    let idx = $(this).data('index');
+        let row = addedRows[idx];
+
+    frappe.confirm(`Remove vendor <b>${row.vendor}</b>?`, () => {
+
+
+        frappe.call({
+            method: "vgjewellry.rate_cut.delete_rate_cut_summary",
+            args: {
+                summary_id: row.name
+            },
+            callback: function () {
+
+                addedRows.splice(idx, 1);
+
+                render_table();   // re-render UI
+
+            }
+        });
+
+    });
+
+});
 };
