2cecd2f221001ab9d6baa7e9d36f3cff98016abd
mitesh <miteshthakur7@gmail.com>
2026-07-16 19:19:00 +0530

    c

--- vgjewellry/master_api.py
+++ vgjewellry/master_api.py
@@ -53,10 +53,14 @@ def get_variety_from_item(item):
     
 @frappe.whitelist(allow_guest=True)
 def get_parent_variety_from_item(item):
+    #query ='''
+    #SELECT variety_id, variety_name, item_trade_mst_id FROM tabOrnate_Variety_Master AS vm WHERE FIND_IN_SET((SELECT item_trade_mst_id  FROM tabOrnate_Item_Master  WHERE item_mst_id = %s or item_mst_id=''), vm.item_trade_mst_id) > 0 and type='Parent' ;
+    #'''
     query ='''
-    SELECT variety_id, variety_name, item_trade_mst_id FROM tabOrnate_Variety_Master AS vm WHERE FIND_IN_SET((SELECT item_trade_mst_id  FROM tabOrnate_Item_Master  WHERE item_mst_id = %s), vm.item_trade_mst_id) > 0 and type='Parent' ;
+    SELECT variety_id, variety_name, item_trade_mst_id FROM tabOrnate_Variety_Master AS vm where type='Parent' ;
     '''
-    data = frappe.db.sql(query, item, as_dict=True)
+    #data = frappe.db.sql(query, item, as_dict=True)
+    data = frappe.db.sql(query, [], as_dict=True)
     variety_ids = [row['variety_id'] for row in data]
 
     return variety_ids
